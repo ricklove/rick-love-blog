57e828d51bfc4c430664c2070996b65edd0435f5
Rick Love <rick@toldpro.com>
2020-05-16 12:54:14 -0500

    Copy Generated Query

--- gatsby-site/src/components/blog-post.tsx
+++ gatsby-site/src/components/blog-post.tsx
@@ -3,6 +3,45 @@ import { useStaticQuery, graphql } from 'gatsby';
 import { Header } from './header';
 import './layout.css';
 
+
+// Copy of Generated Types
+
+// type MarkdownRemark = Node & {
+//     readonly id: Scalars['ID'];
+//     readonly frontmatter: Frontmatter;
+//     readonly excerpt: Maybe<Scalars['String']>;
+//     readonly rawMarkdownBody: Maybe<Scalars['String']>;
+//     readonly fileAbsolutePath: Maybe<Scalars['String']>;
+//     readonly html: Maybe<Scalars['String']>;
+//     readonly htmlAst: Maybe<Scalars['JSON']>;
+//     readonly excerptAst: Maybe<Scalars['JSON']>;
+//     readonly headings: Maybe<ReadonlyArray<Maybe<MarkdownHeading>>>;
+//     readonly timeToRead: Maybe<Scalars['Int']>;
+//     readonly tableOfContents: Maybe<Scalars['String']>;
+//     readonly wordCount: Maybe<MarkdownWordCount>;
+//     readonly parent: Maybe<Node>;
+//     readonly children: ReadonlyArray<Node>;
+//     readonly internal: Internal;
+// };
+
+// type Frontmatter = {
+//     readonly title: Scalars['String'];
+//     readonly author: Maybe<Scalars['String']>;
+//     readonly date: Scalars['Date'];
+//     readonly path: Scalars['String'];
+//     readonly tags: Maybe<ReadonlyArray<Scalars['String']>>;
+//     readonly excerpt: Maybe<Scalars['String']>;
+//     readonly coverImage: Maybe<File>;
+// };
+
+// type BlogPostTemplateQ2Query = { readonly markdownRemark: Maybe<(
+//     Pick<MarkdownRemark, 'id' | 'html' | 'excerpt'>
+//     & { readonly frontmatter: (
+//       Pick<Frontmatter, 'title' | 'date' | 'path' | 'author' | 'excerpt' | 'tags'>
+//       & { readonly coverImage: Maybe<{ readonly childImageSharp: Maybe<{ readonly fluid: Maybe<GatsbyImageSharpFluidFragment> }> }> }
+//     ) }
+//   )> };
+
 export const BlogPost = ({ children }: { children: ReactNode }) => {
     const data = useStaticQuery<GatsbyTypes.BlogPostTemplateQ2Query>(graphql`
         query BlogPostTemplateQ2 {
