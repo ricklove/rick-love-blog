5cc9472b716601a72372d0a43201bfa192166d71
Rick Love <rick@toldpro.com>
2020-05-15 19:51:52 -0500

    Run Eslint on all files

--- gatsby-site/src/pages/page-3.tsx
+++ gatsby-site/src/pages/page-3.tsx
@@ -1,18 +1,16 @@
 // Gatsby supports TypeScript natively!
-import React from "react"
-import { PageProps, Link } from "gatsby"
+import React from "react";
+import { PageProps, Link } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 // Because if we are forced to use export default, it should be nameless (only for pages)
 export default (props: PageProps) => (
   <Layout>
-    <SEO title="Page 3" />
+    <SEO title='Page 3' />
     <h1>Hi from the 3rd  page</h1>
     <p>Welcome to page 3 ({props.path})</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to='/'>Go back to the homepage</Link>
   </Layout>
-)
-
-
+);
