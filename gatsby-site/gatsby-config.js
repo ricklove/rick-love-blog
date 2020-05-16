9bd2550f0aa0bad503aa2dda54781d68eac82123
Rick Love <rick@toldpro.com>
2020-05-15 22:01:35 -0500

    FIX Metadata

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `blog_rick_love`,
+    description: `It's about development mostly`,
+    copyrights: '2020 Copyright Rick Love',
+    author: `Rick Love`,
   },
   plugins: [
     `gatsby-plugin-typegen`,
